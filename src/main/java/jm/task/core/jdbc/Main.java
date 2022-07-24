a3b684146baabfc90473107c1c9080f295672e83
mixadora <m.o.davronov@gmail.com>
2022-07-24 19:13:25 +0800

    Task 3.1.1.3

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,19 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
+    private final static UserService userService = new UserServiceImpl();
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        userService.createUsersTable();
+        userService.saveUser("Mukhammad", "Davronov", (byte) 28);
+        userService.saveUser("Elon", "Musk", (byte) 51);
+        userService.saveUser("Tom", "Cruise", (byte) 60);
+        userService.saveUser("Iron", "Man", (byte) 57);
+        userService.removeUserById(2);
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
