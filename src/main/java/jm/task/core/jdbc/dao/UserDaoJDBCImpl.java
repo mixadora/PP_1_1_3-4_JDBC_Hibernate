acec50e60a6005c9f6a5d5db97dff1fb32383a14
mixadora <m.o.davronov@gmail.com>
2022-07-25 20:46:28 +0800

    исправил

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -22,55 +22,68 @@ public class UserDaoJDBCImpl implements UserDao {
 
     }
 
-    public void createUsersTable() {
+    public void createUsersTable() throws SQLException {
         try (Statement statement = conn.createStatement()) {
+            conn.setAutoCommit(false);
             statement.executeUpdate("CREATE TABLE IF NOT EXISTS users " +
                     "(id BIGINT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255), last_name VARCHAR(255), age INT)");
             conn.commit();
-            conn.rollback ();
         } catch (SQLException e) {
             e.printStackTrace();
+            conn.rollback ();
+        } finally {
+            conn.setAutoCommit(true);
         }
     }
 
-    public void dropUsersTable() {
+    public void dropUsersTable() throws SQLException {
         try (Statement statement = conn.createStatement()) {
+            conn.setAutoCommit(false);
             statement.executeUpdate("DROP TABLE IF EXISTS users");
             conn.commit();
-            conn.rollback ();
         } catch (SQLException e) {
             e.printStackTrace();
+            conn.rollback ();
+        } finally {
+            conn.setAutoCommit(true);
         }
     }
 
-    public void saveUser(String name, String lastName, byte age) {
+    public void saveUser(String name, String lastName, byte age) throws SQLException {
         try (PreparedStatement pstm = conn.prepareStatement("INSERT INTO users (name, last_name, age) VALUES (?, ?, ?)")) {
+            conn.setAutoCommit(false);
             pstm.setString(1, name);
             pstm.setString(2, lastName);
             pstm.setByte(3, age);
             pstm.executeUpdate();
             conn.commit();
-            conn.rollback ();
         } catch (SQLException e) {
             e.printStackTrace();
+            conn.rollback ();
+        } finally {
+            conn.setAutoCommit(true);
         }
     }
 
-    public void removeUserById(long id) {
+    public void removeUserById(long id) throws SQLException {
         try (PreparedStatement pstm = conn.prepareStatement("DELETE FROM users WHERE id = ?")) {
+            conn.setAutoCommit(false);
             pstm.setLong(1, id);
             pstm.executeUpdate();
             conn.commit();
-            conn.rollback ();
         } catch (SQLException e) {
             e.printStackTrace();
+            conn.rollback ();
+        } finally {
+            conn.setAutoCommit(true);
         }
     }
 
-    public List<User> getAllUsers() {
+    public List<User> getAllUsers() throws SQLException {
         List<User> users = new ArrayList<>();
 
         try (ResultSet resultSet = conn.createStatement().executeQuery("SELECT * FROM users")) {
+            conn.setAutoCommit(false);
             while(resultSet.next()) {
                 User user = new User(resultSet.getString("name"),
                         resultSet.getString("last_name"), resultSet.getByte("age"));
@@ -78,21 +91,26 @@ public class UserDaoJDBCImpl implements UserDao {
                 users.add(user);
             }
             conn.commit();
-            conn.rollback ();
         } catch (SQLException e) {
             e.printStackTrace();
+            conn.rollback ();
+        } finally {
+            conn.setAutoCommit(true);
         }
 
         return users;
     }
 
-    public void cleanUsersTable() {
+    public void cleanUsersTable() throws SQLException {
         try (Statement statement = conn.createStatement()) {
+            conn.setAutoCommit(false);
             statement.executeUpdate("TRUNCATE TABLE users");
             conn.commit();
-            conn.rollback ();
         } catch (SQLException e) {
             e.printStackTrace();
+            conn.rollback ();
+        } finally {
+            conn.setAutoCommit(true);
         }
     }
 }
